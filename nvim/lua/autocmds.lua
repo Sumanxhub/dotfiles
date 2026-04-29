729406e306bdc523fa340603265a9ef34aa0c675
Sumanxhub <sumanbh0329@gmail.com>
2026-04-29 16:41:35 +0530

    created custom groups for autocmds

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,10 +1,15 @@
 require "nvchad.autocmds"
 
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- Create a custom group to prevent duplicate execution on reload
+local user_cmds = augroup("UserCustomAutocmds", { clear = true })
 
 -- Automatically exit Neovim if the only remaining window is NvimTree.
 -- This prevents getting stuck with a single NvimTree pane after closing files.
 autocmd("BufEnter", {
+  group = user_cmds,
   callback = function()
     if vim.fn.winnr "$" == 1 and vim.bo.filetype == "NvimTree" then
       vim.cmd "quit"
@@ -14,15 +19,31 @@ autocmd("BufEnter", {
 
 -- Highlight yanked text
 autocmd("TextYankPost", {
+  group = user_cmds,
   callback = function()
-    vim.highlight.on_yank { timeout = 1000 }
+    vim.highlight.on_yank { timeout = 300 }
   end,
 })
 
--- Disable auto-comments on 'o'
-autocmd("FileType", {
+-- Disable auto-comments on 'o' and 'enter'
+autocmd("BufEnter", {
+  group = user_cmds,
   pattern = "*",
   callback = function()
-    vim.opt.formatoptions:remove { "o" }
+    vim.opt.formatoptions:remove { "c", "r", "o" }
   end,
 })
+
+-- Hyprlang LSP
+-- autocmd({ "BufReadPre", "BufNewFile" }, {
+--   group = user_cmds,
+--   pattern = { "*.hl", "hypr*.conf" },
+--   callback = function()
+--     vim.bo.filetype = "hyprlang"
+--     vim.lsp.start {
+--       name = "hyprlang",
+--       cmd = { "hyprls" },
+--       root_dir = vim.fn.getcwd(),
+--     }
+--   end,
+-- })
